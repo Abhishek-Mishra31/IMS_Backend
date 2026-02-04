6142a50d096edbeffa981bfc9b0eb285aa2a7346
Abhishek Mishra <Abhishekbelaganj0609@gmail.com>
2026-02-04 17:29:10 +0530

    Removed extra_space

--- src/modules/inventory/cloudinary.service.ts
+++ src/modules/inventory/cloudinary.service.ts
@@ -7,20 +7,19 @@ export class CloudinaryService {
         return new Promise((resolve, reject) => {
             const uploadStream = cloudinary.uploader.upload_stream(
                 {
-                    folder: 'inventory', // Organize images in a folder
+                    folder: 'inventory',
                     resource_type: 'image',
                 },
                 (error, result) => {
                     if (error) return reject(error);
                     if (!result) return reject(new Error('Upload failed'));
-                    resolve(result.secure_url); // Returns the image URL
+                    resolve(result.secure_url);
                 },
             );
             streamifier.createReadStream(file.buffer).pipe(uploadStream);
         });
     }
     async deleteImage(imageUrl: string): Promise<void> {
-        // Extract public_id from the URL
         const parts = imageUrl.split('/');
         const filename = parts[parts.length - 1];
         const publicId = `inventory/${filename.split('.')[0]}`;
